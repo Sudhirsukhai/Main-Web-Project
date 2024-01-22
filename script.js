cfe448ef43760adf93f9cdd474db6a10a0ec9d11
Glitch (glitch-hello-website) <none>
2024-01-22 23:50:02 +0000

    😀🎖 Checkpoint ./script.js:62997810/191

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.addEventListener("load",addListeners)
 
 function addListeners(){
-  document.getElementById("btnHealthPage").addListener("click",ToHealth)
-  document.getElementById("btnInterCalc").addListener("click",ToCalc)
-  document.getElementById("btnEduGame").addListener("click",ToGame)
-  document.getElementById("btnSearchEng").addListener("click",ToHealth)
+  document.getElementById("btnHealthPage").addEventListener("click",ToHealth)
+  document.getElementById("btnInterCalc").addEventListener("click",ToCalc)
+  document.getElementById("btnEduGame").addEventListener("click",ToGame)
+  document.getElementById("btnSearchEng").addEventListener("click",ToHealth)
 }
 
 function ToHealth(){
-  window.location.href = "Health.html";
+  window.location.replace("https://health-page.glitch.me/");
 }
 
 function ToCalc(){
